f2b3f615bbd6c2a712181a2a75d7a7b3a3e846c2
seuli07 <goc13030@gmail.com>
2023-04-12 18:43:22 +0900

    포스트 목록 페이지와 상세 페이지에 태그 기능 추가하기

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 
 admin.site.register(Post)
@@ -7,5 +7,8 @@ admin.site.register(Post)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
 
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
+
 admin.site.register(Category, CategoryAdmin)
-# Register your models here.
+admin.site.register(Tag, TagAdmin)
